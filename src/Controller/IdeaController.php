ed7e478a4942195b1946d9586fe9cb7ee2067c45
AurelOconnell <aurelienoconnell@gmail.com>
2023-06-05 15:12:58 +0200

    basic form added

--- src/Controller/IdeaController.php
+++ src/Controller/IdeaController.php
@@ -9,19 +9,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class IdeaController extends AbstractController
 {
     #[Route('/idea', name: 'new_idea')]
     public function index(Request $request, IdeaRepository $ideaRepository): Response
     {
-        $idea = new Idea(); 
+        $idea = new Idea();
 
         // Create the form linked with $idea :
         $form = $this->createForm(IdeaType::class, $idea);
         // Get the data from HTTP request :
         $form->handleRequest($request);
-        // Form submitted ? 
+        // Form submitted ?
         if ($form->isSubmitted()) {
             $ideaRepository->save($idea, true);
             return $this->redirectToRoute('idea_index');
--- src/Form/IdeaType.php
+++ src/Form/IdeaType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-
 class IdeaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
