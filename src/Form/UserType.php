cfd1494b9d237bca68fc71292ad8a770a0bbf596
AurelOconnell <aurelienoconnell@gmail.com>
2023-06-09 10:33:15 +0200

    fully fonctional user edit page

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +14,22 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
+            ->add('email', TextType::class)
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => true,
+                'expanded' => true,
+                'choices'  => [
+                  'Contributor' => 'ROLE_CONTRIBUTOR',
+                  'Admin' => 'ROLE_ADMIN',
+                ],
+            ])
             ->add('password')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
             ->add('service')
-            ->add('office')
-            ->add('position')
+            ->add('office', TextType::class)
+            ->add('position', TextType::class)
         ;
     }
 
