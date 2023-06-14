ab9452c9b8aff6ae95febacd5202c15f61799ebe
Spolito24 <baptisterenier69@gmail.com>
2023-06-14 19:18:15 +0200

    a user can now modify is profile picture on is profile page and only on is profile page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,41 +16,25 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/user', name: 'user_')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
+    #[Route('/{id}', name: 'show', methods: ['GET', 'POST'])]
+    public function show(
+        User $user,
+        Request $request,
+        UserRepository $userRepository,
+        FileUploader $fileUploader
+    ): Response {
+        $pictureFile = $request->files->get('user_picture');
 
-    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        if ($pictureFile) {
+            $pictureFilename = $fileUploader->upload($pictureFile);
+            $user->setPictureFileName($pictureFilename);
         }
+        $userRepository->save($user, true);
 
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
         return $this->render('user/profil.html.twig', [
             'user' => $user,
         ]);
     }
-
     #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, User $user, UserRepository $userRepository): Response
     {
@@ -57,6 +42,6 @@ class UserController extends AbstractController
             $userRepository->remove($user, true);
         }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('admin_users', [], Response::HTTP_SEE_OTHER);
     }
 }
