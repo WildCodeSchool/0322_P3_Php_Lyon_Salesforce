f627f4abce4454c4150756a1e52f485975effbb3
AurelOconnell <aurelienoconnell@gmail.com>
2023-06-15 17:33:43 +0200

    removing useless uses in ideaType

--- src/Form/IdeaType.php
+++ src/Form/IdeaType.php
@@ -3,11 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Idea;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
