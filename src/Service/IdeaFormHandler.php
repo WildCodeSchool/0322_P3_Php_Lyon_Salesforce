decb2ccde2162c6e082c0636538896f80e1e89ff
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-06-18 16:25:16 +0200

    IdeaFormHandler was created as a service to lighten the idea_new controller.

---
+++ src/Service/IdeaFormHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Idea;
+use App\Entity\User;
+use App\Form\IdeaType;
+use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IdeaFormHandler extends AbstractController
+{
+    public function formHandler(): array
+    {
+
+        /** @var User $user */
+        $user = $this->getUser();
+        $date = new DateTimeImmutable();
+        $publicationDate = $date->setDate(intval(date('Y')), intval(date('m')), intval(date('d')));
+
+        $idea = new Idea();
+        $form = $this->createForm(IdeaType::class, $idea);
+
+        $idea->setPublicationDate($publicationDate);
+        $idea->setAuthor($user);
+
+         return [
+        'form' => $form,
+        'idea' => $idea
+         ];
+    }
+}
