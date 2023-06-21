fb5f12db0367c0381f8ec370c91c784d79876bc4
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-06-21 17:13:54 +0200

    Security was added ADMIN can delete ideas while users only their own

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -2,21 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\IdeaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class AjaxController extends AbstractController
 {
     #[Route('/ArchivesIdea/{idIdea}', name: 'archives_idea')]
     public function archivesIdea(int $idIdea, IdeaRepository $ideaRepository): Response
     {
-        $idea = $ideaRepository->find($idIdea);
-        $idea->setArchived(true);
-        $ideaRepository->save($idea, true);
 
 
-        return new Response(status: 200);
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $idea = $ideaRepository->find($idIdea);
+            $idea->setArchived(true);
+            $ideaRepository->save($idea, true);
+
+            return new Response(status: 200);
+        } elseif ($this->isGranted('ROLE_CONTRIBUTOR')) {
+            /** @var User $user */
+            $user = $this->getUser();
+
+            $userId = $user->getId();
+
+            $idea = $ideaRepository->findOneBy([
+                'id' => $idIdea,
+                'author' => $userId
+            ]);
+
+            $idea->setArchived(true);
+            $ideaRepository->save($idea, true);
+
+
+            return new Response(status: 200);
+        }
+
+        return new Response(status: 403);
     }
 }
