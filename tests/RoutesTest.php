a07dea5cd10b63dee1968e00c54aff1ac14e57b6
Spolito24 <baptisterenier69@gmail.com>
2023-06-21 14:10:36 +0200

    finish smoke test

---
+++ tests/RoutesTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\UserRepository;
+use Gitonomy\Git\Admin;
+use PhpParser\Node\Expr\Cast\Array_;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RoutesTest extends WebTestCase
+{
+    private array $allRoute = [
+        ['/'],
+        ['/idea/new'],
+        ['/idea/MyOffice'],
+        ['/idea/MyDepartment'],
+        ['/user/1'],
+        ['/admin/users'],
+        ['/admin/users/1/edit'],
+    ];
+
+    private array $userRoute = [
+        ['/'],
+        ['/idea/new'],
+        ['/idea/MyOffice'],
+        ['/idea/MyDepartment'],
+        ['/user/1'],
+    ];
+
+    private array $adminRoute = [
+        ['/admin/users'],
+        ['/admin/users/1/edit'],
+    ];
+
+    public function testLogingPage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    //--------------- USER ROUTES ---------------//
+
+    /**
+     * @dataProvider userProvideRoutes
+     */
+    public function testUserRoute(string $route): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('contributor@sf.com');
+
+        $client->loginUser($testUser);
+
+        $client->request('GET', $route);
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function userProvideRoutes(): array
+    {
+        return $this->userRoute;
+    }
+
+    /**
+     * @dataProvider userUnaccessibleRoutes
+     */
+    public function testUserUnaccessibleRoute(string $route): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('contributor@sf.com');
+
+        $client->loginUser($testUser);
+
+        $client->request('GET', $route);
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function userUnaccessibleRoutes(): array
+    {
+        return $this->adminRoute;
+    }
+
+    //----------------- ADMIN ROUTES -----------------//
+
+    /**
+     * @dataProvider adminProvideRoutes
+     */
+    public function testAdminRoute(string $route): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('superadmin@sf.com');
+
+        $client->loginUser($testUser);
+
+        $client->request('GET', $route);
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function adminProvideRoutes(): array
+    {
+        return $this->allRoute;
+    }
+}
