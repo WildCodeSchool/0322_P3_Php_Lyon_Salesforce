dbb44517ee2c2137f7fcc18880d65f39459282c3
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-06-23 10:32:17 +0200

    Need to fix the redirection after delete

--- assets/deleteIdea.js
+++ assets/deleteIdea.js
@@ -30,3 +30,40 @@ for(const deleteIdeaBtn of deleteIdeaBtns) {
         ;
     });
 }
+
+
+
+
+
+
+const deleteBtnShow = document.getElementsByClassName('deleteBtnShow')[0];
+
+
+deleteBtnShow.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    fetch(deleteBtnShow.getAttribute('href'))
+        .then(response => {
+            if (response.status === 200) {
+
+                    
+                const confirmation = confirm("Etes vous sûr de vouloir supprimer votre idée?");
+                if (confirmation) {
+                    location.replace('http://localhost:8000');
+                    const flashMessage = document.createElement('div');
+                    flashMessage.classList.add('flash-message');
+                    flashMessage.textContent = 'Votre idée a été supprimé.';
+                    document.body.appendChild(flashMessage);
+        
+
+                    setTimeout(() => {
+                        flashMessage.remove();
+                    }, 2500);
+                }
+
+            } else {
+                alert("Erreur");
+            }
+        })
+    ;
+});
