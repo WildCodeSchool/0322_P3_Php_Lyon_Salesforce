6eac05a51b9ab67b959c559f1e69ac78d887310c
AurelOconnell <aurelienoconnell@gmail.com>
2023-06-26 10:47:25 +0200

    adding slack token in .env + function createChannel in SlackService

--- src/Controller/SlackController.php
+++ src/Controller/SlackController.php
@@ -21,7 +21,20 @@ class SlackController extends AbstractController
     {
         $channelName = 'testcreatechannel'; // Set the channel name to 'channelname' (replace with desired channel name)
         // Put in slug cf doc symfony -> automatically create a channel from an idea
-        $channel = $slackService->createChannel($channelName); 
+        $channel = $slackService->createChannel($channelName);
         // Call the createChannel method of SlackService with the specified channel name
+
+        if ($channel['ok']) {
+            $channelId = $channel['channel']['id']; // Extract the channel ID from the response
+            $message = "New channel created: {$channelName} (ID: {$channelId}).";
+            // Create a success message with the channel name and ID
+        } else {
+            $error = $channel['error']; // Extract the error message from the response
+            $message = "Failed to create channel: {$error}."; // Create an error message with the error details
+        }
+
+        return $this->render('slack/create_channel.html.twig', [
+            'message' => $message, // Pass the message to the 'slack/create_channel.html.twig' template
+        ]);
     }
 }
--- src/Service/SlackService.php
+++ src/Service/SlackService.php
@@ -1,3 +1,30 @@
 <?php
 
 namespace App\Service;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class SlackService
+{
+    public $token;
+    public $client;
+
+    public function createChannel(string $channelName): array
+    {
+        $this->token = $_ENV['SLACK_OAUTH_TOKEN']; // Get the Slack OAuth token from the environment variables
+        $this->client = HttpClient::create([
+            'base_uri' => 'https://slack.com/api/conversations.create', // Set the base URI for the HTTP client
+            'header' => [
+                'Authorization' => 'Bearer' . $this->token, // Set the authorization header with the OAuth token
+                'Content-type' => 'application/json', // Set the content type header to JSON
+            ],
+        ]);
+        $response = $this->client->request('POST', 'conversations.create', [
+            'body' => json_encode([
+                'name' => $channelName, // Set the channel name in the request body
+            ]),
+        ]);
+
+        return $this->handleResponse($response); // Call the private handleResponse method to process the response
+    }
+}
