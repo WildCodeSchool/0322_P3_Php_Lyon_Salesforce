4d13b0851ffe0983d9fbabb37151c8c0e4a298c0
AurelOconnell <aurelienoconnell@gmail.com>
2023-06-26 11:50:19 +0200

    SlackController & SlackService finished

--- src/Controller/SlackController.php
+++ src/Controller/SlackController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\SlackService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,7 +21,7 @@ class SlackController extends AbstractController
     public function createChannel(SlackService $slackService): Response
     {
         $channelName = 'testcreatechannel'; // Set the channel name to 'channelname' (replace with desired channel name)
-        // Put in slug cf doc symfony -> automatically create a channel from an idea
+        // Set in slug cf doc symfony -> automatically create a channel from an idea
         $channel = $slackService->createChannel($channelName);
         // Call the createChannel method of SlackService with the specified channel name
 
--- src/Service/SlackService.php
+++ src/Service/SlackService.php
@@ -2,19 +2,22 @@
 
 namespace App\Service;
 
+use RuntimeException;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SlackService
 {
-    public $token;
-    public $client;
+    public string $token;
+    public HttpClientInterface $client;
 
     public function createChannel(string $channelName): array
     {
         $this->token = $_ENV['SLACK_OAUTH_TOKEN']; // Get the Slack OAuth token from the environment variables
         $this->client = HttpClient::create([
             'base_uri' => 'https://slack.com/api/conversations.create', // Set the base URI for the HTTP client
-            'header' => [
+            'headers' => [
                 'Authorization' => 'Bearer' . $this->token, // Set the authorization header with the OAuth token
                 'Content-type' => 'application/json', // Set the content type header to JSON
             ],
@@ -26,5 +29,18 @@ class SlackService
         ]);
 
         return $this->handleResponse($response); // Call the private handleResponse method to process the response
+    }
+
+    private function handleResponse(ResponseInterface $responseInterface): array
+    {
+        $statusCode = $responseInterface->getStatusCode(); // Get the HTTP status code from the response
+        $content = $responseInterface->toArray(); // Convert the response content to an array
+
+        if ($statusCode !== 200 || !$content['ok']) {
+            throw new RuntimeException('Failed to create channel: ' . $content['error']);
+            // If the status code is not 200 or the 'ok' property is false, throw an exception with the error message
+        }
+
+        return $content; // Return the response content as an array
     }
 }
