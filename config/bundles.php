aafa493474f85e6ec43656c6d7f32bd5f7e3a4f7
Spolito24 <baptisterenier69@gmail.com>
2023-07-05 12:26:30 +0200

    all the idea page show only 6 idea and if you want to see more idea you need to go on another page

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Symfony\UX\Swup\SwupBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Idea;
 use App\Repository\IdeaRepository;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +14,16 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_USER')]
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(IdeaRepository $ideaRepository): Response
+    #[Route('/{page<\d+>}', name: 'app_home')]
+    public function index(IdeaRepository $ideaRepository, int $page = 1): Response
     {
-        $ideas = $ideaRepository->findBy(
-            ['archived' => false],
-            ['publicationDate' => 'DESC']
+        $ideas = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new ArrayAdapter($ideaRepository->findBy(
+                ['archived' => false],
+                ['publicationDate' => 'DESC']
+            )),
+            $page,
+            6
         );
 
         return $this->render('home/index.html.twig', [
