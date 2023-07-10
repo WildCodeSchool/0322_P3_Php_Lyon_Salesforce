9514bcbead9ee9b94b5f0bf0b8e7fdc05d4788e5
Spolito24 <baptisterenier69@gmail.com>
2023-07-10 15:21:42 +0200

    add a progress bar on idea Show

--- src/DataFixtures/IdeaFixtures.php
+++ src/DataFixtures/IdeaFixtures.php
@@ -23,7 +23,7 @@ class IdeaFixtures extends Fixture implements DependentFixtureInterface
         ];
 
 
-        for ($j = 1; $j <= 10; $j++) {
+        for ($j = 1; $j <= 15; $j++) {
             $dummyIdea = new Idea();
             $dummyIdea->setTitle($faker->sentence());
             $dummyIdea->setContent($faker->paragraphs(5, true));
@@ -37,12 +37,18 @@ class IdeaFixtures extends Fixture implements DependentFixtureInterface
             $dummyIdea->setPublicationDate(new DateTimeImmutable($faker->date()));
             $dummyIdea->setAuthor($this->getReference('contributor@sf.com'));
 
+            $loopTurn = rand(4, 20);
+            for ($j = 0; $j < $loopTurn; $j++) {
+                $dummyIdea->addSupporter(
+                    $this->getReference('user_' . $faker->numberBetween(1, 10) . '_Lyon')
+                );
+            }
 
             $manager->persist($dummyIdea);
         }
 
         foreach (OfficeFixtures::OFFICES as $officeLocation) {
-            for ($i = 1; $i <= 10; $i++) {
+            for ($i = 1; $i <= 20; $i++) {
                 $idea = new Idea();
                 $idea->setTitle($faker->sentence());
                 $idea->setContent($faker->paragraphs(5, true));
@@ -55,6 +61,12 @@ class IdeaFixtures extends Fixture implements DependentFixtureInterface
                 $idea->setPublicationDate(new DateTimeImmutable($faker->date()));
                 $idea->setAuthor($this->getReference('user_' . $faker->numberBetween(1, 10) . '_' . $officeLocation));
 
+                $loopTurn = rand(4, 20);
+                for ($j = 0; $j < $loopTurn; $j++) {
+                    $idea->addSupporter(
+                        $this->getReference('user_' . $faker->numberBetween(1, 10) . '_' . $officeLocation)
+                    );
+                }
 
                 $manager->persist($idea);
             }
