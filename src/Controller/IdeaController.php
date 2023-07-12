595b2127e29f4ccf5d485141be0d1036dbe4f0cc
AurelOconnell <aurelienoconnell@gmail.com>
2023-07-12 23:43:53 +0200

    supporters filters working on office and service pages

--- src/Controller/IdeaController.php
+++ src/Controller/IdeaController.php
@@ -177,6 +177,69 @@ class IdeaController extends AbstractController
         ]);
     }
 
+    #[Route('/show/sortedOffice/{order}/{page<\d+>}', name: '_sorting_off')]
+    public function sortIdeaForOffice(IdeaRepository $ideaRepository, string $order, int $page = 1): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $officeId = $user->getWorkplace()->getId();
+
+        $sortOrder = ($order === 'desc') ? 'desc' : 'asc';
+
+        if ($sortOrder === 'desc') {
+            $ideas = $ideaRepository->getIdeasByUserOffice($officeId);
+        } else {
+            $ideas = $ideaRepository->getAscIdeasByUserOffice($officeId);
+        }
+        $ideas = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new ArrayAdapter($ideas),
+            $page,
+            6
+        );
+
+        $pagerfanta = new TwitterBootstrap5View();
+
+        return $this->render('idea/ideasByUserOffice.html.twig', [
+            'ideas' => $ideas,
+            'pagerfanta' => $pagerfanta,
+            'currentOrder' => $order,
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/show/sortedDepartment/{order}/{page<\d+>}', name: '_sorting_dep')]
+    public function sortIdeaForDepartment(IdeaRepository $ideaRepository, string $order, int $page = 1): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $officeId = $user->getWorkplace()->getId();
+        $departmentName = $user->getDepartment();
+
+        $sortOrder = ($order === 'desc') ? 'desc' : 'asc';
+
+        if ($sortOrder === 'desc') {
+            $ideas = $ideaRepository->getIdeasByUserDepartment($officeId, $departmentName);
+        } else {
+            $ideas = $ideaRepository->getAscIdeasByUserDepartment($officeId, $departmentName);
+        }
+        $ideas = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new ArrayAdapter($ideas),
+            $page,
+            6
+        );
+
+        $pagerfanta = new TwitterBootstrap5View();
+
+        return $this->render('idea/ideasByUserDepartment.html.twig', [
+            'ideas' => $ideas,
+            'pagerfanta' => $pagerfanta,
+            'currentOrder' => $order,
+            'user' => $user,
+        ]);
+    }
+
     #[Route('/show/sortedByPopularity/supp/{page<\d+>}', name: '_sorting_supp')]
     public function sortIdeaBySupporters(IdeaRepository $ideaRepository, int $page = 1): Response
     {
@@ -195,6 +258,59 @@ class IdeaController extends AbstractController
             'ideas' => $ideas,
             'pagerfanta' => $pagerfanta,
             'currentOrder' => 'supp',
+        ]);
+    }
+
+    #[Route('/show/sortedByPopularity/supp/office/{page<\d+>}', name: '_sorting_supp_off')]
+    public function sortIdeaBySupportersForOffice(IdeaRepository $ideaRepository, int $page = 1): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $officeId = $user->getWorkplace()->getId();
+
+        // sort ideas by their Supporters' number DESC for the office
+        $ideas = $ideaRepository->getSupportersSortIdeaForOffice($officeId, false);
+
+        $ideas = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new ArrayAdapter($ideas),
+            $page,
+            6
+        );
+
+        $pagerfanta = new TwitterBootstrap5View();
+
+        return $this->render('idea/ideasByUserOffice.html.twig', [
+            'ideas' => $ideas,
+            'pagerfanta' => $pagerfanta,
+            'currentOrder' => 'supp',
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/show/sortedByPopularity/supp/department/{page<\d+>}', name: '_sorting_supp_dep')]
+    public function sortIdeaBySupportersForDepartment(IdeaRepository $ideaRepository, int $page = 1): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $officeId = $user->getWorkplace()->getId();
+        $departmentName = $user->getDepartment();
+
+        // sort ideas by their Supporters' number DESC for the department
+        $ideas = $ideaRepository->getSupportersSortIdeaForDepartment($officeId, $departmentName, false);
+
+        $ideas = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new ArrayAdapter($ideas),
+            $page,
+            6
+        );
+
+        $pagerfanta = new TwitterBootstrap5View();
+
+        return $this->render('idea/ideasByUserDepartment.html.twig', [
+            'ideas' => $ideas,
+            'pagerfanta' => $pagerfanta,
+            'currentOrder' => 'supp',
+            'user' => $user,
         ]);
     }
 }
--- src/Repository/IdeaRepository.php
+++ src/Repository/IdeaRepository.php
@@ -115,6 +115,32 @@ class IdeaRepository extends ServiceEntityRepository
             ->getResult();
     }
 
+    public function getAscIdeasByUserOffice(int $officeId): array
+    {
+        return $this->createQueryBuilder('i')
+            ->select(
+                'i.id',
+                'i.title',
+                'i.content',
+                'o.location',
+                'i.publicationDate',
+                'u.lastname',
+                'u.firstname',
+                'u.pictureFileName'
+            )
+            ->innerJoin('i.author', 'u')
+            ->innerJoin('u.workplace', 'o')
+            ->where('o.id = :officeId')
+            ->andWhere('i.perimeter = :agence')
+            ->andWhere('i.archived = :archived')
+            ->setParameter('archived', false)
+            ->setParameter('officeId', $officeId)
+            ->setParameter('agence', 'Agence')
+            ->orderBy('i.publicationDate', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
     public function getIdeasByUserDepartment(int $officeId, string $departmentName): array
     {
         return $this->createQueryBuilder('i')
@@ -139,6 +165,34 @@ class IdeaRepository extends ServiceEntityRepository
             ->setParameter('service', 'Service')
             ->setParameter('departmentName', $departmentName)
             ->orderBy('i.publicationDate', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getAscIdeasByUserDepartment(int $officeId, string $departmentName): array
+    {
+        return $this->createQueryBuilder('i')
+            ->select(
+                'i.id',
+                'i.title',
+                'i.content',
+                'o.location',
+                'i.publicationDate',
+                'u.lastname',
+                'u.firstname',
+                'u.pictureFileName'
+            )
+            ->innerJoin('i.author', 'u')
+            ->innerJoin('u.workplace', 'o')
+            ->where('o.id = :officeId')
+            ->andWhere('u.department = :departmentName')
+            ->andWhere('i.perimeter = :service')
+            ->andWhere('i.archived = :archived')
+            ->setParameter('archived', false)
+            ->setParameter('officeId', $officeId)
+            ->setParameter('service', 'Service')
+            ->setParameter('departmentName', $departmentName)
+            ->orderBy('i.publicationDate', 'ASC')
             ->getQuery()
             ->getResult();
     }
@@ -198,7 +252,9 @@ class IdeaRepository extends ServiceEntityRepository
             ->innerJoin('u.workplace', 'o')
             ->leftJoin('i.supporters', 's')
             ->where('i.archived = :archived')
+            ->andWhere('i.perimeter = :global')
             ->setParameter('archived', $archived)
+            ->setParameter('global', 'Global')
             ->groupBy('i.id')
             ->orderBy('supportersCount', 'DESC')
             ->getQuery();
@@ -216,6 +272,93 @@ class IdeaRepository extends ServiceEntityRepository
             $idea->pictureFileName = $result['pictureFileName'];
             $ideas[] = $idea;
         }
+        return $ideas;
+    }
+
+    public function getSupportersSortIdeaForOffice(int $officeId, bool $archived = false): array
+    {
+        $query = $this->createQueryBuilder('i')
+            ->select(
+                'i',
+                'COUNT(s) as supportersCount',
+                'o.location',
+                'u.lastname',
+                'u.firstname',
+                'u.pictureFileName'
+            )
+            ->innerJoin('i.author', 'u')
+            ->innerJoin('u.workplace', 'o')
+            ->leftJoin('i.supporters', 's')
+            ->where('i.archived = :archived')
+            ->andWhere('o.id = :officeId')
+            ->andWhere('i.perimeter = :officePerimeter')
+            ->setParameter('archived', $archived)
+            ->setParameter('officeId', $officeId)
+            ->setParameter('officePerimeter', 'Agence')
+            ->groupBy('i.id')
+            ->orderBy('supportersCount', 'DESC')
+            ->getQuery();
+
+        $results = $query->getResult();
+
+        $ideas = [];
+        foreach ($results as $result) {
+            $idea = $result[0];
+            $supportersCount = $result['supportersCount'];
+            $idea->supportersCount = $supportersCount;
+            $idea->location = $result['location'];
+            $idea->lastname = $result['lastname'];
+            $idea->firstname = $result['firstname'];
+            $idea->pictureFileName = $result['pictureFileName'];
+            $ideas[] = $idea;
+        }
+
+        return $ideas;
+    }
+
+    public function getSupportersSortIdeaForDepartment(
+        int $officeId,
+        string $departmentName,
+        bool $archived = false
+    ): array {
+        $query = $this->createQueryBuilder('i')
+            ->select(
+                'i',
+                'COUNT(s) as supportersCount',
+                'o.location',
+                'u.lastname',
+                'u.firstname',
+                'u.pictureFileName'
+            )
+            ->innerJoin('i.author', 'u')
+            ->innerJoin('u.workplace', 'o')
+            ->leftJoin('i.supporters', 's')
+            ->where('i.archived = :archived')
+            ->andWhere('o.id = :officeId')
+            ->andWhere('u.department = :departmentName')
+            ->andWhere('i.perimeter = :service')
+            ->setParameter('archived', $archived)
+            ->setParameter('officeId', $officeId)
+            ->setParameter('departmentName', $departmentName)
+            ->setParameter('service', 'Service')
+            ->groupBy('i.id')
+            ->orderBy('supportersCount', 'DESC')
+            ->getQuery();
+
+        $results = $query->getResult();
+
+        $ideas = [];
+        foreach ($results as $result) {
+            $idea = $result[0];
+            $supportersCount = $result['supportersCount'];
+            $idea->supportersCount = $supportersCount;
+            $idea->location = $result['location'];
+            $idea->lastname = $result['lastname'];
+            $idea->firstname = $result['firstname'];
+            $idea->pictureFileName = $result['pictureFileName'];
+            $ideas[] = $idea;
+        }
+
         return $ideas;
     }
 }
