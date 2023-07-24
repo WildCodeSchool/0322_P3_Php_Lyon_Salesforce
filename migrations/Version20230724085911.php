8a2983a8d3f7bfbc233294b3d8b8f69d4bc4b944
AurelOconnell <aurelienoconnell@gmail.com>
2023-07-24 12:26:02 +0200

    correcting previous migration and deleting the new one

--- migrations/Version20230724085911.php
+++ migrations/Version20230724085911.php
@@ -20,7 +20,7 @@ final class Version20230724085911 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE password password VARCHAR(100) NOT NULL, CHANGE firstname firstname VARCHAR(100) NOT NULL, CHANGE lastname lastname VARCHAR(100) NOT NULL, CHANGE department department VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE password password VARCHAR(255) NOT NULL, CHANGE firstname firstname VARCHAR(100) NOT NULL, CHANGE lastname lastname VARCHAR(100) NOT NULL, CHANGE department department VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
